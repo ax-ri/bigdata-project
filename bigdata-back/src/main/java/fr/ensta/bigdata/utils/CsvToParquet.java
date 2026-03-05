5f36bcc433516684c9a592154d160c4a212554c6
ElfieMoBo <elfie.mobo.31570@gmail.com>
2026-03-05 10:23:09 +0100

    (back) feat: csv to parquet

---
+++ bigdata-back/src/main/java/fr/ensta/bigdata/utils/CsvToParquet.java
@@ -0,0 +1,47 @@
+package fr.ensta.bigdata.utils;
+
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+
+/**
+ * Utility class to convert a file from CSV to Parquet.
+ */
+public class CsvToParquet {
+    public static void main(String[] args) {
+        final String inputName = args[0];
+        final String outputName = args[1];
+
+        // Build Spark Session
+        SparkSession spark = SparkSession.builder()
+                .appName("CSV To Parquet")
+                .master("local")
+                .config("spark.sql.parquet.int96RebaseModeInWrite", "CORRECTED")
+                .getOrCreate();
+
+        // Read the CSV file into a Data Frame
+        Dataset<Row> csvDs = spark.read()
+                .option("header", "true") // keep CSV header for field names
+                .option("inferSchema", "true")
+                .option("quote", "\"") // needed for proper escape handling (quotes and commas inside a value)
+                .option("escape", "\"") // same as above
+                .csv(inputName);
+
+        csvDs.show(5);
+        System.out.println("[info] The dataframe has " + csvDs.count() + " rows.");
+
+        // Write the Data Frame as Parquet
+        csvDs.to(csvDs.schema()).write().mode("overwrite").parquet(outputName);
+
+        System.out.println("[info] Successfully written Parquet File to " + outputName);
+
+        // Reads a Parquet back into Data Frame to ensure the file is valid
+        Dataset<Row> parquetDs = spark.read()
+                .option("inferSchema", "true")
+                .parquet(outputName);
+        parquetDs.show(5);
+        parquetDs.printSchema();
+
+        System.out.println("[info] The Parquet dataframe has " + parquetDs.count() + " rows.");
+    }
+}
