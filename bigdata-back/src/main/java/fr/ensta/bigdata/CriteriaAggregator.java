e385257628cc10c0bdb22ee084ecd1049b7a25b7
ElfieMoBo <elfie.mobo.31570@gmail.com>
2026-03-24 19:12:47 +0100

    (back) refactor: main

--- bigdata-back/src/main/java/fr/ensta/bigdata/CriteriaAggregator.java
+++ bigdata-back/src/main/java/fr/ensta/bigdata/CriteriaAggregator.java
@@ -2,7 +2,6 @@ package fr.ensta.bigdata;
 
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
-import org.apache.spark.sql.SparkSession;
 import org.apache.spark.sql.expressions.Window;
 import org.apache.spark.sql.expressions.WindowSpec;
 
@@ -12,62 +11,6 @@ import static org.apache.spark.sql.functions.*;
  * Create aggregated data for a given criteria, for further analysis.
  */
 public class CriteriaAggregator {
-    private final SparkSession spark;
-    private final String inputParquetPath;
-
-    public CriteriaAggregator(SparkSession spark, String inputParquetPath) {
-        this.spark = spark;
-        this.inputParquetPath = inputParquetPath;
-    }
-
-    /**
-     * Load dataset from disk with the correct schema.
-     *
-     * @return dataset
-     */
-    private Dataset<Row> readDataset() {
-        // get the dataset from the spark session
-        Dataset<Row> ds = this.spark.read()
-                .format("parquet")
-                .option("inferSchema", "true")
-                .load(this.inputParquetPath);
-
-        // cast the column to the right type (schema)
-        return ds.select(
-                        col("id").cast("long"),
-                        col("title").cast("string"),
-                        col("rank").cast("long"),
-                        col("date").cast("date"),
-                        col("artist").cast("string"),
-                        col("url").cast("string"),
-                        col("region").cast("string"),
-                        col("chart").cast("string"),
-                        col("trend").cast("string"),
-                        col("streams").cast("long"),
-                        col("track_id").cast("string"),
-                        col("album").cast("string"),
-                        col("popularity").cast("double"),
-                        col("duration_ms").cast("double"),
-                        col("explicit").cast("boolean"),
-                        col("release_date").cast("date"),
-                        col("available_markets").cast("string"),
-                        col("af_danceability").cast("double"),
-                        col("af_energy").cast("double"),
-                        col("af_key").cast("double"),
-                        col("af_loudness").cast("double"),
-                        col("af_mode").cast("boolean"),
-                        col("af_speechiness").cast("double"),
-                        col("af_acousticness").cast("double"),
-                        col("af_instrumentalness").cast("double"),
-                        col("af_liveness").cast("double"),
-                        col("af_valence").cast("double"),
-                        col("af_tempo").cast("double"),
-                        col("af_time_signature").cast("double")
-                )
-                // convert null values to 0
-                .na().fill(0);
-    }
-
     /**
      * Normalize the values of the column named `metric` such that the minimum is at 0 and the maximum is at 1.
      *
@@ -89,11 +32,12 @@ public class CriteriaAggregator {
      * @param songCount number of most popular song to consider
      * @return final dataset
      */
-    public Dataset<Row> aggregate(String criteria, int year, int songCount) {
+    public Dataset<Row> aggregate(Dataset<Row> baseDs, String criteria, int year, int songCount) {
         WindowSpec w = Window.partitionBy("region").orderBy(desc("streams"));
         return normalizeValue(
-                readDataset()
+                baseDs
                         .filter("YEAR(date) = '" + year + "'")
+                        .cache()
                         .select("region", criteria, "streams")
                         .withColumn("rank", dense_rank().over(w))
                         .filter(col("rank").$less$eq(songCount))
--- bigdata-back/src/main/java/fr/ensta/bigdata/Main.java
+++ bigdata-back/src/main/java/fr/ensta/bigdata/Main.java
@@ -27,15 +27,24 @@ public final class Main {
         final String countryCsvPath = args[1];
         final String outputDir = args[2];
 
-        CriteriaAggregator agg = new CriteriaAggregator(spark, inputParquetPath);
+        Dataset<Row> baseDs = spark.read().parquet(inputParquetPath);
+        CriteriaAggregator criteriaAgg = new CriteriaAggregator();
+        SuccessAggregator successAgg = new SuccessAggregator();
         CountryJoiner joiner = new CountryJoiner(spark, countryCsvPath);
 
-        for (String criteria : new String[]{"duration_ms", "af_danceability", "af_energy", "af_key", "af_loudness", "af_speechiness", "af_acousticness", "af_liveness", "af_valence", "af_tempo"}) {
-            for (int year = 2017; year <= 2021; year++) {
-                Dataset<Row> ds = agg.aggregate(criteria, year, 10);
+        for (int year = 2017; year <= 2021; year++) {
+            for (String criteria : new String[]{"duration_ms", "af_danceability", "af_energy", "af_key", "af_loudness", "af_speechiness", "af_acousticness", "af_liveness", "af_valence", "af_tempo"}) {
+                // Create dataset for criteria over region (map)
+                Dataset<Row> ds = criteriaAgg.aggregate(baseDs, criteria, year, 10);
                 ds = joiner.join(ds, "region");
                 writeDatasetAsCsv(ds, Path.of(outputDir, criteria + "-" + year).toString());
             }
+        }
+
+        for (String criteria : new String[]{"duration_ms", "af_danceability", "af_energy", "af_key", "af_loudness", "af_speechiness", "af_acousticness", "af_liveness", "af_valence", "af_tempo"}) {
+            // Create dataset for criteria over stream
+            Dataset<Row> ds = successAgg.aggregate(baseDs, criteria);
+            writeDatasetAsCsv(ds, Path.of(outputDir, "success-" + criteria).toString());
         }
 
         spark.stop();
