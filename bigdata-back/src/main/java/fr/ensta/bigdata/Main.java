24f74b7f9ec8e0cbbd18ae1d7d40e0b48ece8d0a
ElfieMoBo <elfie.mobo.31570@gmail.com>
2026-04-04 12:37:57 +0200

    (back) feat: clustering

--- bigdata-back/src/main/java/fr/ensta/bigdata/Main.java
+++ bigdata-back/src/main/java/fr/ensta/bigdata/Main.java
@@ -1,10 +1,14 @@
 package fr.ensta.bigdata;
 
+import fr.ensta.bigdata.utils.Clustering;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.functions;
 
 import java.nio.file.Path;
+
+import static org.apache.spark.sql.functions.*;
 
 public final class Main {
     private static void writeDatasetAsCsv(Dataset<Row> ds, String filePath) {
@@ -16,6 +20,8 @@ public final class Main {
     }
 
     public static void main(String[] args) {
+
+
         SparkSession spark = SparkSession.builder()
                 .master("local")
                 .appName("Spotify Parquet")
@@ -23,28 +29,84 @@ public final class Main {
                 .config("spark.sql.parquet.binaryAsString", "true")
                 .getOrCreate();
 
+        spark.sparkContext().setLogLevel("WARN");
+
         final String inputParquetPath = args[0];
         final String countryCsvPath = args[1];
         final String outputDir = args[2];
+        final int choice = Integer.parseInt(args[3]);
 
         Dataset<Row> baseDs = spark.read().parquet(inputParquetPath);
         CriteriaAggregator criteriaAgg = new CriteriaAggregator();
         SuccessAggregator successAgg = new SuccessAggregator();
         CountryJoiner joiner = new CountryJoiner(spark, countryCsvPath);
+        Clustering clustering = new Clustering();
 
-        for (int year = 2017; year <= 2021; year++) {
-            for (String criteria : new String[]{"duration_ms", "af_danceability", "af_energy", "af_key", "af_loudness", "af_speechiness", "af_acousticness", "af_liveness", "af_valence", "af_tempo"}) {
-                // Create dataset for criteria over region (map)
-                Dataset<Row> ds = criteriaAgg.aggregate(baseDs, criteria, year, 10);
-                ds = joiner.join(ds, "region");
-                writeDatasetAsCsv(ds, Path.of(outputDir, criteria + "-" + year).toString());
+
+        // Questions:
+        // - How does musical trends vary over the years?
+        // - Can countries be clustered by similar music taste?
+        // - Which musical characteristics (tempo, energy, etc.) give the best chance of success?
+
+        if  (choice == 1) {
+            // --> CSV file for rendering map colored by criteria (1)
+            for (int year = 2017; year <= 2021; year++) {
+                for (String criteria : new String[]{"duration_ms", "af_danceability", "af_energy", "af_key", "af_loudness", "af_speechiness", "af_acousticness", "af_liveness", "af_valence", "af_tempo"}) {
+                    Dataset<Row> ds = criteriaAgg.aggregate(baseDs, criteria, year, 10);
+                    ds = joiner.join(ds, "region");
+                    writeDatasetAsCsv(ds, Path.of(outputDir, criteria + "-" + year).toString());
+                }
             }
-        }
-
-        for (String criteria : new String[]{"duration_ms", "af_danceability", "af_energy", "af_key", "af_loudness", "af_speechiness", "af_acousticness", "af_liveness", "af_valence", "af_tempo"}) {
-            // Create dataset for criteria over stream
-            Dataset<Row> ds = successAgg.aggregate(baseDs, criteria);
-            writeDatasetAsCsv(ds, Path.of(outputDir, "success-" + criteria).toString());
+        } else if (choice == 2) {
+            // --> CSV file for rendering graph of criteria/number of streams (1)
+            for (String criteria : new String[]{"duration_ms", "af_danceability", "af_energy", "af_key", "af_loudness", "af_speechiness", "af_acousticness", "af_liveness", "af_valence", "af_tempo"}) {
+                Dataset<Row> ds = successAgg.aggregate(baseDs, criteria);
+                ds.show();
+                writeDatasetAsCsv(ds, Path.of(outputDir, "success-" + criteria).toString());
+            }
+        } else if  (choice == 3) {
+            // --> CSV file to cluster countries by their musical taste (2)
+            Dataset<Row> clusteredDs = clustering.cluster(baseDs, 100);
+            clusteredDs.show();
+        } else if  (choice == 4) {
+            // --> CSV file for each criterion to get the statistics of a hit (3)
+            int n = 1000;
+            Dataset<Row> clusteredDs = clustering.cluster(baseDs, 10000);
+            Dataset<Row> ciDs = null;
+            for (String criteria : new String[] {"af_danceability", "af_energy", "af_key", "af_loudness", "af_speechiness", "af_acousticness", "af_instrumentalness", "af_valence", "af_tempo", "af_time_signature"}) {
+                Dataset<Row> pureDS = baseDs
+                        .select("id", "streams", criteria);
+                Dataset<Row> statsDs = clusteredDs
+                        .join(pureDS, array_contains(clusteredDs.col("ids"), baseDs.col("id")))
+                        .select("prediction", "streams", criteria)
+                        .groupBy("prediction")
+                        .agg(
+                                functions.sum("streams").alias("streams"),
+                                functions.avg(criteria).alias(criteria)
+                        )
+                        .orderBy(col("streams").desc())
+                        .limit(n)
+                        .agg(
+                                functions.median(col(criteria)).alias("mean"),
+                                functions.stddev(col(criteria)).alias("stddev")
+                        )
+                        .withColumn("criteria", lit(criteria));
+                Dataset<Row> errorDs = statsDs.select(
+                        col("mean"),
+                        col("criteria"),
+                        // confidence interval (95%)
+                        expr("mean - 1.96 * (stddev / sqrt(" + n + "))").alias("ci_lower"),
+                        expr("mean + 1.96 * (stddev / sqrt(" + n + "))").alias("ci_upper")
+                );
+                if (ciDs == null) {
+                    ciDs = errorDs;
+                } else {
+                    ciDs = ciDs.union(errorDs);
+                }
+            }
+            writeDatasetAsCsv(ciDs, Path.of(outputDir, "ci").toString());
+        } else {
+            System.out.println("[test]");
         }
 
         spark.stop();
---
+++ bigdata-back/src/main/java/fr/ensta/bigdata/utils/Clustering.java
@@ -0,0 +1,91 @@
+package fr.ensta.bigdata.utils;
+
+import org.apache.spark.ml.clustering.KMeans;
+import org.apache.spark.ml.clustering.KMeansModel;
+import org.apache.spark.ml.evaluation.ClusteringEvaluator;
+import org.apache.spark.ml.feature.VectorAssembler;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.functions;
+
+import static org.apache.spark.sql.functions.*;
+
+
+public final class Clustering {
+
+    /**
+     * Normalize the values of the column named `metric` such that the minimum is at 0 and the maximum is at 1.
+     *
+     * @param ds source dataset
+     * @return modified dataset
+     */
+    private Dataset<Row> normalizeValue(Dataset<Row> ds, String criteria) {
+        final double max = ds.agg(max(criteria)).alias("max").first().getDouble(0);
+        final double min = ds.agg(min(criteria)).alias("min").first().getDouble(0);
+        final double n = max == min ? 1 : max - min;
+        return ds.withColumn(criteria, col(criteria).minus(min).divide(n));
+    }
+
+    /**
+     * Prepare the dataset to be clustered (regroup by title, normalize values, create vector)
+     * @param dataset   source dataset
+     * @return  modified dataset
+     */
+    private Dataset<Row> prepareDataset(Dataset<Row> dataset) {
+        // Regroup identical music (title/artist)
+        Dataset<Row> ds = dataset
+                .groupBy("title")
+                .agg(
+                        functions.collect_list("id").alias("ids"),
+                        functions.avg("af_danceability").alias("af_danceability"),
+                        functions.avg("af_energy").alias("af_energy"),
+                        functions.avg("af_key").alias("af_key"),
+                        functions.avg("af_loudness").alias("af_loudness"),
+                        functions.avg("af_speechiness").alias("af_speechiness"),
+                        functions.avg("af_acousticness").alias("af_acousticness"),
+                        functions.avg("af_instrumentalness").alias("af_instrumentalness"),
+                        functions.avg("af_valence").alias("af_valence"),
+                        functions.avg("af_tempo").alias("af_tempo"),
+                        functions.avg("af_time_signature").alias("af_time_signature"));
+
+        // Normalize each audio feature
+        for (String criteria : new String[]{"af_danceability", "af_energy", "af_key", "af_loudness", "af_speechiness", "af_acousticness", "af_instrumentalness", "af_valence", "af_tempo", "af_time_signature"}){
+            dataset = normalizeValue(ds, criteria);
+            ds = dataset;
+        }
+
+        // Select the cluster vector
+        VectorAssembler vec = new VectorAssembler("clustering")
+                .setInputCols(new String[]{"af_danceability", "af_energy", "af_key", "af_loudness", "af_speechiness", "af_acousticness", "af_instrumentalness", "af_valence", "af_tempo", "af_time_signature"})
+                .setOutputCol("features");
+
+        return vec
+                .transform(ds)
+                .select("ids", "title", "features");
+    }
+
+    /**
+     * Cluster music by their audio features
+     * @param dataset   source dataset
+     * @param nbClusters    number of clusters
+     * @return  clustered dataset [ids | prediction ]
+     */
+    public Dataset<Row> cluster(Dataset<Row> dataset, int nbClusters) {
+        // Prepares dataset
+        Dataset<Row> clusteredDs = prepareDataset(dataset);
+
+        // Trains a k-means model.
+        KMeans kmeans = new KMeans().setK(nbClusters).setSeed(1L);
+        KMeansModel model = kmeans.fit(clusteredDs);
+
+        // Make predictions
+        Dataset<Row> predictions = model.transform(clusteredDs);
+
+        // Evaluate clustering by computing Silhouette score (need to be positif)
+        ClusteringEvaluator evaluator = new ClusteringEvaluator();
+        double silhouette = evaluator.evaluate(predictions);
+        assert silhouette > 0;
+
+        return predictions.select("ids", "prediction");
+    }
+}
